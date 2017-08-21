2067fecbb0f40d132c769d3290acae9941849d80
JNHM <jeffreynghm@gmail.com>
2017-08-21 16:05:32 +0800

    version walk through with Ophelia

--- Corp_FX_Mining_Download_SZ.py
+++ Corp_FX_Mining_Download_SZ.py
@@ -21,7 +21,7 @@ import socket #catch gaddrinfo error
 
 
      
-def scrapReports_SZSE(link,stockcode,driver,res):
+def scrapReports_SZSE(link,stockcode,driver,res,f):
     #make sure the download function works
 
     #change pdf default behavior on firefox to be download about:preferences#applications
@@ -61,10 +61,12 @@ def scrapReports_SZSE(link,stockcode,driver,res):
     match = False
     for element in elements:
         
-        match = pattern.match(element.text, re.UNICODE)
-        #RE not working
+        #TODO: RE not working - need to fix it - ok now we ignore it
+        match = pattern.match(element.text, re.UNICODE)  #should use search
+        
         #if match:
-        docName = element.text
+        #need to download the relevant file
+        #docName = element.text --- no need
         newpage = element.get_attribute('href')
         head, tail = os.path.split(newpage)
         data={'stockcode':stockcode,'filename':tail,'fileDescription':docName,'KeywordSet':'','link':newpage,'set':['']}
@@ -72,11 +74,13 @@ def scrapReports_SZSE(link,stockcode,driver,res):
         
         #element.click()
         print(tail)
-        
+
         try:
-            urllib.request.urlretrieve(newpage,".\\PDF\\"+tail)
+            urllib.request.urlretrieve(newpage,".\\PDF_SZ\\"+tail)
         except Exception:
             print("can't download "+newpage)
+            continue
+        f.write(stockcode+'\t'+tail+'\n')
             
     return res
 
@@ -116,8 +120,10 @@ def calScore(f,res,filetxt,resScore):
     #for tk in words:
     #    print(tk)
     return resScore
-def main():
 
+def main():
+    
+    #get parameters for the program and create necessry variables
     pathname = os.path.dirname(os.path.abspath(__file__))
     #create dataframe that store all output data
     data = {
@@ -128,7 +134,8 @@ def main():
         'fxScore':0,
         'KeywordSet':['']
         }
-
+    recordtxt = 'record_SZ.txt'
+    f = open(recordtxt,'w+')
     res = pd.DataFrame(data)
     resScore = pd.DataFrame(data)
     
@@ -142,14 +149,15 @@ def main():
     #gecko driver can be downloaded from github and place in any place, as long as it is specified below
     driver = webdriver.Firefox(profile,executable_path='C:\\Jeffrey Ng\\gecko\\geckodriver.exe')
     link = 'http://disclosure.szse.cn/m/search0425.jsp'
-    stockcodes = '.\StockCodes_SZ.txt'
-    for stockcode in open(stockcodes, "r",encoding = 'utf-8'):
-        res=scrapReports_SZSE(link,stockcode,driver,res)
-    res=scrapReports_SHSE(link,stockcode,driver,res)
+    stockcodes_file = '.\StockCodes_SZ.txt'
+    for stockcode in open(stockcodes_file, "r",encoding = 'utf-8'):
+        res=scrapReports_SZSE(link,stockcode,driver,res,f)
+    
     res.to_csv("scrapResult_SZ.txt",encoding = 'utf-8',sep='\t')
     resScore.to_csv("scrapResultScore_SZ.txt",encoding = 'utf-8')
     res_M=res.merge(resScore,how='inner',on='filename')
     res_M.to_csv("scrapResult_M_SZ.txt",encoding = 'utf-8',sep='\t')
+    f.close()
 
 if __name__ == '__main__':
   main()
